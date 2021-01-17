a88061ce6d3f14f18ede409ca7825885b4546f6c
Ayush Sharma <sharma.ayush84444@gmail.com>
2021-01-17 15:23:16 +0530

    Review working perfect

--- Utility/databaseUtil.py
+++ Utility/databaseUtil.py
@@ -6,45 +6,94 @@ class reviewObj:
         self.email = data[0]
         self.name = data[1]
         self.title = data[2]
-        self.review = data[3]
-        self.image = data[4]
+        self.company = data[3]
+        self.review = data[4]
+        self.image = data[5]
+        self.id = data[6]
+
+class authObj:
+    def __init__(self, data):
+        self.username = data[0]
+        self.password = data[1]
 
 def createTable():
     sql_create_review_table = """ CREATE TABLE IF NOT EXISTS reviews (
                                     Email text PRIMARY KEY,
                                     Name text NOT NULL,
                                     Title text NOT NULL,
+                                    Company text NOT NULL,
                                     Review text NOT NULL,
                                     Image text
                                     ); """
-
-def updateData():
-    sql = ''' UPDATE reviews
-              SET image = ?
-              WHERE email = ?'''
     conn = sqlite3.connect( 'database\database.db' )
     c = conn.cursor()
-    c.execute( sql, review )
-    c.commit()
+    c.execute( sql_create_review_table )
+    conn.commit()
 
-def insertData( review ):
-    sql = ''' INSERT INTO reviews(Name,Email,Title,Review,Image)
-              VALUES(?,?,?,?,?) '''
+def createAuthTable():
+    sql_create_auth_table = """ CREATE TABLE IF NOT EXISTS auth (
+                                    Username text PRIMARY KEY,
+                                    Password_hash text NOT NULL
+                                    ); """
+    conn = sqlite3.connect( 'database\database.db' )
+    c = conn.cursor()
+    c.execute( sql_create_auth_table )
+    conn.commit()
+
+def insertUserNamePassword():
+    sql = ''' INSERT INTO auth(Username,Password_hash)
+              VALUES(?,?) '''
+    conn = sqlite3.connect( 'database\database.db' )
+    c = conn.cursor()
+    c.execute( sql, ( 'portfolioUser@sf.com', "b'gAAAAABgAr08c0VWa5WMAnI3smwOOelf1qmr8S-cMPatfNf22lHvHyZVPGYA3AKnr9HWmPhp4C8D2UNxIf67Nuw0f9OVqENxvw=='" ) )
+    conn.commit()
+
+def updateData( review ):
+    sql = ''' UPDATE auth
+              SET Password_hash = ?
+              WHERE Username = ?'''
     conn = sqlite3.connect( 'database\database.db' )
     c = conn.cursor()
     c.execute( sql, review )
     conn.commit()
 
+def insertData( review ):
+    sql = ''' INSERT INTO reviews(Name,Email,Title,Company,Review,Image)
+              VALUES(?,?,?,?,?,?) '''
+    conn = sqlite3.connect( 'database\database.db' )
+    c = conn.cursor()
+    c.execute( sql, review )
+    conn.commit()
+
+def getUserPass():
+    conn = sqlite3.connect( 'database\database.db' )
+    sql = 'SELECT * FROM auth'
+    c = conn.cursor()
+    c.execute(sql)
+    rows = c.fetchall()
+    auth = authObj( rows[0] )
+    return auth
+
 
 def queryData():
     conn = sqlite3.connect( 'database\database.db' )
-    print(conn)
     sql = 'SELECT * FROM reviews'
     c = conn.cursor()
-    print(c)
     c.execute(sql)
     rows = c.fetchall()
     reviews = [] 
+    l = list(range(0, len(rows) ))
     for row in rows:
+        if l:
+            row += l.pop(),
         reviews.append( reviewObj( row ) )
     return reviews
+
+
+def deleteReviewRecord( email ):
+    sql = 'DELETE FROM reviews WHERE Email = ?;'
+    conn = sqlite3.connect( 'database\database.db' )
+    c = conn.cursor()
+    c.execute( sql, (email,) )
+    conn.commit()
+    
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, BooleanField, StringField
+from wtforms import TextAreaField, StringField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from werkzeug.utils import secure_filename
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from wtforms.validators import InputRequired
-from Utility.databaseUtil import queryData, insertData
+from Utility.databaseUtil import queryData, insertData, reviewObj, authObj, getUserPass, deleteReviewRecord
+from json import dumps, loads
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -15,6 +17,7 @@ class AForm(FlaskForm):
     rName = StringField(validators=[InputRequired()])
     rTitle = StringField(validators=[InputRequired()])
     rEmail = StringField(validators=[InputRequired()])
+    rComp = StringField(validators=[InputRequired()])
     rFile = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['jpg','jpeg','png'])])
     rReview = TextAreaField('Text', validators=[InputRequired()], render_kw={"rows": 4, "cols": 50})
 
@@ -31,17 +34,61 @@ def renderIndex():
     form = AForm()
     return render_template("index.html", form=form)
 
-@app.route("/saveReview/", methods=['POST'])
-def saveReview():
+@app.route('/sendForApproval', methods=['POST', 'GET'])
+def sendForApproval():
     form = AForm()
+    print( str(form.rName.data) )
+    print( str(form.rFile.data) )
     filename = secure_filename(form.rFile.data.filename)
     form.rFile.data.save('static/images/' + filename)
-    insertData(( str(form.rName.data), str(form.rEmail.data), 
-                    str(form.rTitle.data), str(form.rReview.data), 
-                    'images/' + filename ))
-    rows = queryData()    
-    return render_template("index.html", form=form, rows=rows)
+    reviewData = { "Name":str(form.rName.data),
+                   "Email__c":str(form.rEmail.data),
+                   "Title__c":str(form.rTitle.data),
+                   "Company__c":str(form.rComp.data),
+                   "Comment__c":str(form.rReview.data),
+                   "Image__c":'images/' + filename }
+    response = requests.post('https://ayu-dev-sharma-developer-edition.ap5.force.com/portfolio/services/apexrest/createReview',
+                data= dumps( reviewData, indent = 4 ),
+                headers={'content-type':'application/json'}
+                 )
+                 
+    print( response.status_code )
+    print( response.content )
+    
+    return redirect("/")
+    
 
+@app.route("/saveReview", methods=['POST'])
+def saveReview():
+    form = AForm()
+    authObj = getUserPass()
+    print(request.args.get('username'))
+    print(request.args.get('password'))
+    if( authObj.username == request.args.get('username') and authObj.password == request.args.get('password') ):
+        reviewObj = request.json
+        print( reviewObj )
+        insertData(( reviewObj['Name'], reviewObj['Email__c'], reviewObj['Title__c'], 
+                     reviewObj['Company__c'], reviewObj['Comment__c'], reviewObj['Image__c']))
+
+        response = {'message':'Review added on the page.'}
+        return response, 200
+    return {'message':'Username or Password did not match'}, 401
+
+
+@app.route("/deleteReview", methods=['POST'])
+def deleteReview():
+    form = AForm()
+    authObj = getUserPass()
+    print(request.args.get('username'))
+    print(request.args.get('password'))
+    if( authObj.username == request.args.get('username') and authObj.password == request.args.get('password') ):
+        reviewObj = request.json
+        print( reviewObj )
+        deleteReviewRecord( reviewObj['Email__c'] )
+
+        response = {'message':'Review delete/Rejected successfully'}
+        return response, 200
+    return {'message':'Username or Password did not match'}, 401
 
 if __name__ == '__main__':
     app.run(debug=True)
